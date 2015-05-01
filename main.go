6a00258c281f0e0ec4927bc998566cb6e24e713b
Daisuke Matsushima <damassima@gmail.com>
2015-05-01 12:34:23 +0900

    merge flag into params

--- main.go
+++ main.go
@@ -30,16 +30,23 @@ type Params struct {
 
 func main() {
 
-	p := &Params{}
-	parse_flags(p)
+	// priority : flag > .env (file) > env
+
+	f := &Params{}
+	parse_flags(f)
 	err_read := godotenv.Load()
 	if err_read != nil {
 		log.Fatalf("error: %v", err_read)
 	}
+
+	p := &Params{}
+	merge_params(p, f)
+
 	SENDGRID_USERNAME := os.Getenv("SENDGRID_USERNAME")
 	SENDGRID_PASSWORD := os.Getenv("SENDGRID_PASSWORD")
 
 	email := sendgrid.NewMail()
+
 	for _, to := range strings.Split(p.tos, ",") {
 		email.AddTo(to)
 	}
@@ -59,21 +66,49 @@ func main() {
 	}
 }
 
-func parse_flags(p *Params) {
-	flag.StringVar(&p.sendgridUsername, "sendgrid_username", "", "usage sendgrid_username")
-	flag.StringVar(&p.sendgridPassword, "sendgrid_password", "", "usage sendgrid_password")
-	flag.StringVar(&p.tos, "tos", "", "usage tos")
-	flag.StringVar(&p.recipients, "recipients", "", "usage recipients")
-	flag.StringVar(&p.ccs, "ccs", "", "usage ccs")
-	flag.StringVar(&p.ccRecipients, "cc_recipients", "", "usage cc_recipients")
-	flag.StringVar(&p.bccs, "bccs", "", "usage bccs")
-	flag.StringVar(&p.bccRecipients, "bcc_recipients", "", "usage bcc_recipients")
-	flag.StringVar(&p.from, "from", "", "usage from")
-	flag.StringVar(&p.fromName, "from_name", "", "usage from_name")
-	flag.StringVar(&p.replyTo, "reply_to", "", "usage reply_to")
-	flag.StringVar(&p.subject, "subject", "", "usage subject")
-	flag.StringVar(&p.text, "text", "", "usage text")
-	flag.StringVar(&p.html, "html", "", "usage html")
-	flag.StringVar(&p.attachmentFilePath, "attachment_file_path", "", "usage attachment_file_path")
+func parse_flags(f *Params) {
+	flag.StringVar(&f.sendgridUsername, "sendgrid_username", "", "usage sendgrid_username")
+	flag.StringVar(&f.sendgridPassword, "sendgrid_password", "", "usage sendgrid_password")
+	flag.StringVar(&f.tos, "tos", "", "usage tos")
+	flag.StringVar(&f.recipients, "recipients", "", "usage recipients")
+	flag.StringVar(&f.ccs, "ccs", "", "usage ccs")
+	flag.StringVar(&f.ccRecipients, "cc_recipients", "", "usage cc_recipients")
+	flag.StringVar(&f.bccs, "bccs", "", "usage bccs")
+	flag.StringVar(&f.bccRecipients, "bcc_recipients", "", "usage bcc_recipients")
+	flag.StringVar(&f.from, "from", "", "usage from")
+	flag.StringVar(&f.fromName, "from_name", "", "usage from_name")
+	flag.StringVar(&f.replyTo, "reply_to", "", "usage reply_to")
+	flag.StringVar(&f.subject, "subject", "", "usage subject")
+	flag.StringVar(&f.text, "text", "", "usage text")
+	flag.StringVar(&f.html, "html", "", "usage html")
+	flag.StringVar(&f.attachmentFilePath, "attachment_file_path", "", "usage attachment_file_path")
 	flag.Parse()
+}
+
+func merge_params(p *Params, f *Params) {
+	p.sendgridUsername = coalesce(f.sendgridUsername, os.Getenv("SENDGRID_USERNAME"))
+	p.sendgridPassword = coalesce(f.sendgridPassword, os.Getenv("SENDGRID_PASSWORD"))
+	p.tos = coalesce(f.tos, os.Getenv("TOS"))
+	p.recipients = coalesce(f.recipients, os.Getenv("RECIPIENTS"))
+	p.ccs = coalesce(f.ccs, os.Getenv("CCS"))
+	p.ccRecipients = coalesce(f.ccRecipients, os.Getenv("CC_RECIPIENTS"))
+	p.bccs = coalesce(f.bccs, os.Getenv("BCCS"))
+	p.bccRecipients = coalesce(f.bccRecipients, os.Getenv("BCC_RECIPIENTS"))
+	p.from = coalesce(f.from, os.Getenv("FROM"))
+	p.fromName = coalesce(f.fromName, os.Getenv("FROM_NAME"))
+	p.replyTo = coalesce(f.replyTo, os.Getenv("REPLY_TO"))
+	p.subject = coalesce(f.subject, os.Getenv("SUBJECT"))
+	p.text = coalesce(f.text, os.Getenv("TEXT"))
+	p.html = coalesce(f.html, os.Getenv("HTML"))
+	p.attachmentFilePath = coalesce(f.attachmentFilePath, os.Getenv("ATTACHMENT_FILE_PATH"))
+}
+
+func coalesce(v1 string, v2 string) string {
+	ret := ""
+	if v1 == "" {
+		ret = v2
+	} else {
+		ret = v1
+	}
+	return ret
 }
